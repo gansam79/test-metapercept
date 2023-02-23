80d4e5bd20d9c1d6fb4fdf11fa8d87dfbfffbe14
Ganesh Samgir <89154568+gansam79@users.noreply.github.com>
2023-02-23 14:23:49 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+// alert();
+BasicTechnicalWriting
+// {
+// alert('This course focuses on understanding the basics of technical writing and developing technical writing skills. In this course you will learn techniques to analyze writing contexts which helps you to produce effective, clean, and reader-center documents efficiently.');
+// }
